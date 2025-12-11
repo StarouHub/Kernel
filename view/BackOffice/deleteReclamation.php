8894e0c8d79785f231c9510340f5d37bbaf0da9b
cyrine <votre-email@example.com>
2025-12-11 09:51:01 +0100

    Notif-Stats-AI

--- view/BackOffice/deleteReclamation.php
+++
@@ -1,8 +0,0 @@
-<?php
-require '../../Verification.php';
-require '../../Controller/ReclamationController.php';
-if(isset($_GET['id'])){
-    deleteReclamation($_GET['id']);
-}
-header("Location: bookList.php");
-?>
